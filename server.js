bba6b8f5c13dfb6d5ed37ed1452d5275f9b85b34
Aq3rty <nikitaser215@gmail.com>
2026-04-12 06:58:08 +0300

    Логика и фиксы багов
    Убил баг с перезагрузкой страницы Повесил жесткий e.preventDefault() на форму отправки. Теперь при нажатии на Enter или кнопку «Отправить» страница больше не моргает и не стирает текст
    
    Дизайн и интерфейс
    
    Новая палитра: Полностью перевел цвета на Telegram Dark Theme (глубокие темно-синие, сланцевые и серо-голубые оттенки)

--- server.js
+++ server.js
@@ -4,48 +4,33 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-let msgHistory = []; // Общий чат
-let users = {}; 
-
 app.use(express.static('public'));
+
+let users = {}; 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-    users[socket.id] = { 
-        name: 'Принц_' + socket.id.substr(0, 3), 
-        avatar: `https://ui-avatars.com/api/?name=P&background=8b5cf6&color=fff` 
-    };
-    
-    socket.emit('load history', msgHistory);
-    io.emit('update users', users);
-
-    socket.on('update profile', (data) => {
-        if(data.name) users[socket.id].name = data.name;
-        if(data.avatar) users[socket.id].avatar = data.avatar;
+    socket.on('auth', (userData) => {
+        users[socket.id] = {
+            name: userData.name || 'User',
+            username: userData.username || 'id' + socket.id.substr(0,4),
+            avatar: userData.avatar || `https://ui-avatars.com/api/?name=${userData.name}&background=334155&color=fff`
+        };
         io.emit('update users', users);
     });
 
     socket.on('message', (data) => {
         const fullMsg = {
-            sender: users[socket.id].name,
-            sender_id: socket.id,
-            avatar: users[socket.id].avatar,
+            sender: users[socket.id]?.name || 'System',
+            sender_user: users[socket.id]?.username || 'anon',
             text: data.text,
             recipient: data.recipient,
             time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
         };
-
-        if (data.recipient === 'global') {
-            msgHistory.push(fullMsg);
-            if (msgHistory.length > 50) msgHistory.shift();
-            io.emit('new message', fullMsg); 
-        } else {
-            socket.to(data.recipient).emit('new message', fullMsg);
-            socket.emit('new message', fullMsg);
-        }
+        io.emit('new message', fullMsg); 
     });
 
     socket.on('disconnect', () => {
@@ -55,6 +40,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+http.listen(PORT, () => console.log(`Prince Server: Port ${PORT}`));
