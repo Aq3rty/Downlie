5357334d6be473ee927cdea13b744a0451d75077
Aq3rty <nikitaser215@gmail.com>
2026-04-15 08:20:54 +0300

    update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const io = require('socket.io')(http);
 const path = require('path');
 
 app.use(express.static(__dirname));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 let users = {}; 
 let messages = [];
@@ -12,56 +13,27 @@ let messages = [];
 io.on('connection', (socket) => {
     socket.on('auth', (userData) => {
         socket.username = userData.username;
-        if (users[socket.username]) {
-            users[socket.username].online = true;
-            users[socket.username].socketId = socket.id;
-        } else {
-            users[socket.username] = {
-                ...userData,
-                socketId: socket.id,
-                online: true
-            };
-        }
+        users[socket.username] = { ...userData, online: true };
         socket.emit('init_data', { messages, users });
         io.emit('update_users', users);
     });
 
-    socket.on('message', (msgData) => {
+    socket.on('message', (msg) => {
         const fullMsg = {
-            ...msgData,
-            id: Date.now(),
-            time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
-            read: false,
-            // Если это файл, у него будут поля fileData и fileName
-            type: msgData.fileData ? 'file' : 'text' 
+            ...msg,
+            id: 'm_' + Date.now(),
+            time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
         };
         messages.push(fullMsg);
         io.emit('new_message', fullMsg);
     });
 
-    socket.on('read_chat', (data) => {
-        messages.forEach(m => {
-            if (m.recipient === data.me && m.sender_user === data.withWhom) {
-                m.read = true;
-            }
-        });
-        io.emit('messages_read', { by: data.me, from: data.withWhom });
-    });
-
-    socket.on('admin_clear', () => {
-        if (socket.username === 'admin') {
-            messages = [];
-            io.emit('init_data', { messages, users });
-        }
-    });
-
     socket.on('disconnect', () => {
-        if (socket.username && users[socket.username]) {
+        if (users[socket.username]) {
             users[socket.username].online = false;
             io.emit('update_users', users);
         }
     });
 });
 
-const PORT = 3000;
-http.listen(PORT, () => console.log(`PRINCE ELITE Server started on port ${PORT}`));
+http.listen(3000, () => console.log('PRINCE ELITE v5.0 Server Online'));
